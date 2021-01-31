37403a8ffc25a14b25b56a2eaa9d001344fd407b
Javokhir <javokhirsadirov@gmail.com>
2021-01-31 22:53:10 +0500

    fist commit

---
+++ Assets/Scripts/BallBehaviour.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BallBehaviour : MonoBehaviour
+{
+    private LineRenderer line;
+
+    Camera cam;
+
+    public int ballNumber;
+
+    public TextAsset movementTrajectoryForBall1;
+    private float[] pointX; private float[] pointY; private float[] pointZ;
+    public float speed;
+    private int currentPosition = 1;
+
+    public float firstClickTime = 0f;
+
+    [SerializeField]
+    private bool isMoving = false;
+
+    public float getX(int index){
+        return pointX[index];
+    }
+    public float getY(int index){
+        return pointY[index];
+    }
+    public float getZ(int index){
+        return pointZ[index];
+    }
+    public void SetMove(bool move){
+        isMoving = move;
+    }
+    public void SetCurrentPosition(int pos){
+        currentPosition = pos;
+    }
+    public int getCurrentPosition(){
+        return currentPosition;
+    }
+    public void ResetLine(){
+        line.positionCount = 0;
+    }
+    public int getPoints(){
+        return pointX.Length;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+        line.SetPosition(0, transform.position);
+        line.SetPosition(1, transform.position);
+        BallTrajectoryPositions ballWay = JsonUtility.FromJson<BallTrajectoryPositions>(movementTrajectoryForBall1.text);
+
+        pointX = ballWay.x;
+        pointY = ballWay.y;
+        pointZ = ballWay.z;
+
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(isMoving == true){
+            Vector3 nextPoint = new Vector3(pointX[currentPosition],
+                                            pointY[currentPosition],
+                                            pointZ[currentPosition]);
+
+            if(Vector3.Distance(transform.position, nextPoint) > 0.001f){         
+                transform.position = Vector3.MoveTowards(transform.position, nextPoint, speed * Time.deltaTime);
+            }else{
+                line.positionCount++;
+                if(currentPosition!=0)
+                    line.SetPosition(line.positionCount - 1, transform.position);
+
+                currentPosition += 1;
+                
+                if(currentPosition==pointX.Length){
+                    isMoving = false;
+                    currentPosition = 0;
+                }
+                
+            }
+        }        
+    }
+
+    public void ChangeSpeed(float newSpeed){
+        speed = newSpeed*100;
+    }
+
+    private class BallTrajectoryPositions{
+        public float[] x;
+        public float[] y;
+        public float[] z;
+    }
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject[] balls;
+    public Vector3 offset;
+    
+    [SerializeField]
+    private int currentTarget;
+
+    private float yawSpeed = 100f;
+    private float yawInput = 0f;
+
+
+    void Start() {
+        currentTarget = 0;
+    }
+    
+    void Update() {
+        if(Input.GetMouseButtonDown(0)){
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit)){
+                var selection = hit.transform;
+                var selectedBall = selection.GetComponent<BallBehaviour>();
+
+                if(selectedBall != null){
+
+                    if(Time.time - selectedBall.firstClickTime < 0.2f){
+                        selectedBall.transform.position = new Vector3(selectedBall.getX(0), selectedBall.getY(0), selectedBall.getZ(0));
+                        selectedBall.SetMove(false);
+                        selectedBall.ResetLine();
+                        selectedBall.SetCurrentPosition(0);
+                    }else{
+                        if(selectedBall.getCurrentPosition() != selectedBall.getPoints())
+                            selectedBall.SetMove(true);
+                    }
+                    selectedBall.firstClickTime = Time.time;
+
+                    currentTarget = selectedBall.ballNumber;
+                    for(int i=0;i<balls.Length;i++){
+                        balls[i].GetComponent<BallBehaviour>().SetMove(false);
+                    }
+                    balls[currentTarget].GetComponent<BallBehaviour>().SetMove(true);
+                }
+            }
+        }
+
+        yawInput -= Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = balls[currentTarget].transform.position - offset;
+        transform.LookAt(balls[currentTarget].transform.position + Vector3.up);  
+
+        transform.RotateAround(balls[currentTarget].transform.position, Vector3.up, yawInput);
+    }
+
+    public void ChangeTargettoNext(){
+
+        for(int i=0;i<balls.Length;i++){
+            balls[i].GetComponent<BallBehaviour>().SetMove(false);
+        }
+
+        currentTarget = (currentTarget + 1); 
+        currentTarget = Mathf.Clamp(currentTarget, 0, balls.Length-1);
+        balls[currentTarget].GetComponent<BallBehaviour>().SetMove(true);
+
+    }
+
+    public void ChangeTargettoPrev(){
+        for(int i=0;i<balls.Length;i++){
+            balls[i].GetComponent<BallBehaviour>().SetMove(false);
+        }
+        currentTarget = (currentTarget - 1); 
+        currentTarget = Mathf.Clamp(currentTarget, 0, balls.Length-1);
+        balls[currentTarget].GetComponent<BallBehaviour>().SetMove(true);
+
+    }
+}
